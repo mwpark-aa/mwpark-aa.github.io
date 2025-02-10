841b306b69d8f71c6ef53e5243fffe2646c7b76c
mwpark-aa <parkmin614@gmail.com>
2025-02-10 22:28:33 +0900

    add index

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Typography, Box, Link } from "@mui/material";
+import { Container, Typography, Link } from "@mui/material";
 
 const Contact = () => {
     return (
